ddcbd1f9d8c8734e1d667b4c42f9a2579435238e
ksb <developer.ksb@gmail.com>
2025-04-01 14:29:42 +0900

    BugFix : comebine existing code and Glim

--- build.sh
+++ build.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env sh
 colcon build \
   --merge-install \
+  --symlink-install \
   --event-handlers console_cohesion+ \
   --cmake-args \
     -DCMAKE_BUILD_TYPE=Release \
     -GNinja \
-    -DOPENGL_opengl_LIBRARY="/opt/mamba/envs/ros2/lib/libGL.so.1" \
-    -DOPENGL_glx_LIBRARY="/opt/mamba/envs/ros2/lib/libGLX.so.0" \
-    -DQT_HOST_PATH="/opt/mamba/envs/ros2" \
-    -Dnanoflann_DIR="/opt/mamba/share/cmake/nanoflann" \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+    -DCMAKE_INSTALL_PATH=/usr/include \
     -DBUILD_TESTS=OFF \
     -DBUILD_TESTS_PCL=OFF \
     -DBUILD_DEMO=OFF \
@@ -18,7 +16,9 @@ colcon build \
     -DGTSAM_WITH_TBB=OFF \
     -DGTSAM_USE_SYSTEM_EIGEN=ON \
     -DBUILD_WITH_OPENMP=ON \
-    -DBUILD_WITH_CUDA=OFF \
-    -DBUILD_WITH_CUDA_MULTIARCH=OFF \
+    -DBUILD_WITH_CUDA=ON \
+    -DBUILD_WITH_CUDA_MULTIARCH=ON \
     -DBUILD_WITH_VIEWER=OFF \
-    -DBUILD_WITH_MARCH_NATIVE=ON
+    -DBUILD_WITH_MARCH_NATIVE=OFF \
+    $@
+
--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
-ros2 launch clustering_cxx clustering.xml bagfile:=$PWD/../fusion/new/new.db3
+ros2 launch glim_ros glim_launch.xml bagfile:=$PWD/../fusion/new/new.db3
+
